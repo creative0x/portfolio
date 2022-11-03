1a70f6a61334880878c4ee62aaa47a930905c81f
creative0x <creative0x.proton.me>
2022-11-02 21:04:28 -0600

    skills section complete

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -2,8 +2,39 @@ import React from "react";
 
 export default function Portfolio() {
   return (
-    <div className="w-full">
-      <div className="max-w-[1240px] mx-auto px-2 py-16"></div>
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Property Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+            tech="React JS"
+          />
+          <ProjectItem
+            title="Crypto App"
+            backgroundImg={cryptoImg}
+            projectUrl="/crypto"
+            tech="React JS"
+          />
+          <ProjectItem
+            title="Netflix App"
+            backgroundImg={netflixImg}
+            projectUrl="/netflix"
+            tech="React JS"
+          />
+          <ProjectItem
+            title="Twitch UI"
+            backgroundImg={twitchImg}
+            projectUrl="/twitch"
+            tech="Next JS"
+          />
+        </div>
+      </div>
     </div>
   );
 }
